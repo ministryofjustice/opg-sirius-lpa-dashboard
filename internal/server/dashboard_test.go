abe4c1412f9b4e747524d8a08ffb935b820ea3c0
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-05-19 16:17:10 +0100

    Add dashboard handler
    Displays a dashboard of the current users' pending cases when they hit the page at "/"

---
+++ internal/server/dashboard_test.go
@@ -0,0 +1,124 @@
+package server
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ministryofjustice/opg-sirius-lpa-dashboard/internal/sirius"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockDashboardClient struct {
+	myDetails struct {
+		count   int
+		lastCtx sirius.Context
+		data    sirius.MyDetails
+		err     error
+	}
+	casesByAssignee struct {
+		count   int
+		lastCtx sirius.Context
+		lastId  int
+		data    []sirius.Case
+		err     error
+	}
+}
+
+func (m *mockDashboardClient) MyDetails(ctx sirius.Context) (sirius.MyDetails, error) {
+	m.myDetails.count += 1
+	m.myDetails.lastCtx = ctx
+
+	return m.myDetails.data, m.myDetails.err
+}
+
+func (m *mockDashboardClient) CasesByAssignee(ctx sirius.Context, id int) ([]sirius.Case, error) {
+	m.casesByAssignee.count += 1
+	m.casesByAssignee.lastCtx = ctx
+	m.casesByAssignee.lastId = id
+
+	return m.casesByAssignee.data, m.casesByAssignee.err
+}
+
+func TestGetDashboard(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockDashboardClient{}
+	client.myDetails.data = sirius.MyDetails{
+		ID: 14,
+	}
+	client.casesByAssignee.data = []sirius.Case{{
+		ID: 78,
+		Donor: sirius.Donor{
+			ID: 79,
+		},
+	}}
+	template := &mockTemplate{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	err := dashboard(client, template)(w, r)
+	assert.Nil(err)
+
+	assert.Equal(1, client.myDetails.count)
+	assert.Equal(getContext(r), client.myDetails.lastCtx)
+
+	assert.Equal(1, client.casesByAssignee.count)
+	assert.Equal(getContext(r), client.casesByAssignee.lastCtx)
+	assert.Equal(14, client.casesByAssignee.lastId)
+
+	assert.Equal(1, template.count)
+	assert.Equal("page", template.lastName)
+	assert.Equal(dashboardVars{
+		Cases: client.casesByAssignee.data,
+	}, template.lastVars)
+}
+
+func TestGetDashboardMyDetailsError(t *testing.T) {
+	assert := assert.New(t)
+
+	expectedError := errors.New("oops")
+
+	client := &mockDashboardClient{}
+	client.myDetails.err = expectedError
+	template := &mockTemplate{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	err := dashboard(client, template)(w, r)
+	assert.Equal(expectedError, err)
+
+	assert.Equal(1, client.myDetails.count)
+	assert.Equal(getContext(r), client.myDetails.lastCtx)
+
+	assert.Equal(0, client.casesByAssignee.count)
+}
+
+func TestGetDashboardQueryError(t *testing.T) {
+	assert := assert.New(t)
+
+	expectedError := errors.New("oops")
+
+	client := &mockDashboardClient{}
+	client.myDetails.data = sirius.MyDetails{
+		ID: 14,
+	}
+	client.casesByAssignee.err = expectedError
+	template := &mockTemplate{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	err := dashboard(client, template)(w, r)
+	assert.Equal(expectedError, err)
+
+	assert.Equal(1, client.myDetails.count)
+	assert.Equal(getContext(r), client.myDetails.lastCtx)
+
+	assert.Equal(1, client.casesByAssignee.count)
+	assert.Equal(getContext(r), client.casesByAssignee.lastCtx)
+	assert.Equal(14, client.casesByAssignee.lastId)
+}
---
+++ internal/server/server_test.go
@@ -0,0 +1,221 @@
+package server
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/ministryofjustice/opg-sirius-lpa-dashboard/internal/sirius"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockLogger struct {
+	count       int
+	lastRequest *http.Request
+	lastError   error
+}
+
+func (m *mockLogger) Request(r *http.Request, err error) {
+	m.count += 1
+	m.lastRequest = r
+	m.lastError = err
+}
+
+type mockTemplate struct {
+	count    int
+	lastName string
+	lastVars interface{}
+}
+
+func (m *mockTemplate) ExecuteTemplate(w io.Writer, name string, vars interface{}) error {
+	m.count += 1
+	m.lastName = name
+	m.lastVars = vars
+	return nil
+}
+
+func TestNew(t *testing.T) {
+	assert.Implements(t, (*http.Handler)(nil), New(nil, nil, nil, "", "", "", ""))
+}
+
+func TestErrorHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	tmplError := &mockTemplate{}
+
+	wrap := errorHandler(nil, tmplError, "/prefix", "http://sirius")
+	handler := wrap(func(w http.ResponseWriter, r *http.Request) error {
+		w.WriteHeader(http.StatusTeapot)
+		return nil
+	})
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+
+	assert.Equal(http.StatusTeapot, resp.StatusCode)
+	assert.Equal(0, tmplError.count)
+}
+
+func TestErrorHandlerUnauthorized(t *testing.T) {
+	assert := assert.New(t)
+
+	tmplError := &mockTemplate{}
+
+	wrap := errorHandler(nil, tmplError, "/prefix", "http://sirius")
+	handler := wrap(func(w http.ResponseWriter, r *http.Request) error {
+		return sirius.ErrUnauthorized
+	})
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+	assert.Equal(http.StatusFound, resp.StatusCode)
+	assert.Equal("http://sirius/auth", resp.Header.Get("Location"))
+
+	assert.Equal(0, tmplError.count)
+}
+
+func TestErrorHandlerRedirect(t *testing.T) {
+	assert := assert.New(t)
+
+	tmplError := &mockTemplate{}
+
+	wrap := errorHandler(nil, tmplError, "/prefix", "http://sirius")
+	handler := wrap(func(w http.ResponseWriter, r *http.Request) error {
+		return RedirectError("/here")
+	})
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+	assert.Equal(http.StatusFound, resp.StatusCode)
+	assert.Equal("/prefix/here", resp.Header.Get("Location"))
+
+	assert.Equal(0, tmplError.count)
+}
+
+func TestErrorHandlerStatus(t *testing.T) {
+	assert := assert.New(t)
+
+	logger := &mockLogger{}
+	tmplError := &mockTemplate{}
+
+	wrap := errorHandler(logger, tmplError, "/prefix", "http://sirius")
+	handler := wrap(func(w http.ResponseWriter, r *http.Request) error {
+		return StatusError(http.StatusTeapot)
+	})
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	handler.ServeHTTP(w, r)
+
+	resp := w.Result()
+	assert.Equal(http.StatusInternalServerError, resp.StatusCode)
+
+	assert.Equal(1, tmplError.count)
+	assert.Equal(errorVars{SiriusURL: "http://sirius", Code: http.StatusInternalServerError, Error: "418 I'm a teapot"}, tmplError.lastVars)
+
+	assert.Equal(1, logger.count)
+	assert.Equal(r, logger.lastRequest)
+	assert.Equal(StatusError(http.StatusTeapot), logger.lastError)
+}
+
+func TestErrorHandlerStatusKnown(t *testing.T) {
+	for name, code := range map[string]int{
+		"Forbidden": http.StatusForbidden,
+		"Not Found": http.StatusNotFound,
+	} {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			logger := &mockLogger{}
+			tmplError := &mockTemplate{}
+
+			wrap := errorHandler(logger, tmplError, "/prefix", "http://sirius")
+			handler := wrap(func(w http.ResponseWriter, r *http.Request) error {
+				return StatusError(code)
+			})
+
+			w := httptest.NewRecorder()
+			r, _ := http.NewRequest("GET", "/path", nil)
+
+			handler.ServeHTTP(w, r)
+
+			resp := w.Result()
+			assert.Equal(code, resp.StatusCode)
+
+			assert.Equal(1, tmplError.count)
+			assert.Equal(errorVars{SiriusURL: "http://sirius", Code: code, Error: fmt.Sprintf("%d %s", code, name)}, tmplError.lastVars)
+
+			assert.Equal(1, logger.count)
+			assert.Equal(r, logger.lastRequest)
+			assert.Equal(StatusError(code), logger.lastError)
+		})
+	}
+}
+
+func TestGetContext(t *testing.T) {
+	assert := assert.New(t)
+
+	r, _ := http.NewRequest("GET", "/", nil)
+	r.AddCookie(&http.Cookie{Name: "XSRF-TOKEN", Value: "z3tVRZ00yx4dHz3KWYv3boLWHZ4/RsCsVAKbvo2SBNc%3D"})
+	r.AddCookie(&http.Cookie{Name: "another", Value: "one"})
+
+	ctx := getContext(r)
+	assert.Equal(r.Context(), ctx.Context)
+	assert.Equal(r.Cookies(), ctx.Cookies)
+	assert.Equal("z3tVRZ00yx4dHz3KWYv3boLWHZ4/RsCsVAKbvo2SBNc=", ctx.XSRFToken)
+}
+
+func TestGetContextBadXSRFToken(t *testing.T) {
+	assert := assert.New(t)
+
+	r, _ := http.NewRequest("GET", "/", nil)
+	r.AddCookie(&http.Cookie{Name: "XSRF-TOKEN", Value: "%"})
+	r.AddCookie(&http.Cookie{Name: "another", Value: "one"})
+
+	ctx := getContext(r)
+	assert.Equal(r.Context(), ctx.Context)
+	assert.Equal(r.Cookies(), ctx.Cookies)
+	assert.Equal("", ctx.XSRFToken)
+}
+
+func TestGetContextMissingXSRFToken(t *testing.T) {
+	assert := assert.New(t)
+
+	r, _ := http.NewRequest("GET", "/", nil)
+	r.AddCookie(&http.Cookie{Name: "another", Value: "one"})
+
+	ctx := getContext(r)
+	assert.Equal(r.Context(), ctx.Context)
+	assert.Equal(r.Cookies(), ctx.Cookies)
+	assert.Equal("", ctx.XSRFToken)
+}
+
+func TestGetContextForPostRequest(t *testing.T) {
+	assert := assert.New(t)
+
+	r, _ := http.NewRequest("POST", "/", strings.NewReader("xsrfToken=the-real-one"))
+	r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	r.AddCookie(&http.Cookie{Name: "XSRF-TOKEN", Value: "z3tVRZ00yx4dHz3KWYv3boLWHZ4/RsCsVAKbvo2SBNc%3D"})
+	r.AddCookie(&http.Cookie{Name: "another", Value: "one"})
+
+	ctx := getContext(r)
+	assert.Equal(r.Context(), ctx.Context)
+	assert.Equal(r.Cookies(), ctx.Cookies)
+	assert.Equal("the-real-one", ctx.XSRFToken)
+}
