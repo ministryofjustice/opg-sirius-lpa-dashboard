4beafec7be5dac029eca8ac3e6bf2b76ba001d98
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-05-20 09:56:20 +0100

    Improve dashboard tests
    Check that all the data is shown in the table and the link correctly goes to the case page.

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -1,11 +1,18 @@
-describe("The tests run", () => {
+describe("Dashboard", () => {
     beforeEach(() => {
         cy.setCookie("Other", "other");
         cy.setCookie("XSRF-TOKEN", "abcde");
         cy.visit("/", { failOnStatusCode: false });
     });
 
-    it("goes to a page", () => {
-        cy.contains('Your cases');
+    it("shows your cases", () => {
+        cy.title().should('contain', 'Your cases');
+        cy.get('h1').should('contain', 'Your cases');
+
+        const $row = cy.get('table > tbody > tr');
+        $row.should('contain', 'Wilma Ruthman');
+        $row.should('contain', 'HW');
+        $row.should('contain', '14 May 2021');
+        $row.contains('7000-2830-9492').should('have.attr', 'href').should('contain', '/person/17/58');
     });
 });
