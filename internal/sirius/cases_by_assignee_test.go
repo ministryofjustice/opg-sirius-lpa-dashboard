5e09920c2ddf783ddc67fba39747f1fbe9743cb1
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-05-24 17:25:39 +0100

    Add cypress tests
    This requires a non-filtered response from casesByAssignee, which I've added by writing a new test

--- internal/sirius/cases_by_assignee_test.go
+++ internal/sirius/cases_by_assignee_test.go
@@ -32,12 +32,83 @@ func TestCasesByAssignee(t *testing.T) {
 	}{
 		{
 			name:   "OK",
-			status: "Pending",
 			setup: func() {
 				pact.
 					AddInteraction().
 					Given("I have a pending case assigned").
 					UponReceiving("A request to get my cases").
+					WithRequest(dsl.Request{
+						Method: http.MethodGet,
+						Path:   dsl.String("/api/v1/assignees/47/cases"),
+						Query: dsl.MapMatcher{
+							"filter": dsl.String("caseType:lpa,active:true"),
+							"sort":   dsl.String("caseSubType:asc"),
+							"page":   dsl.String("1"),
+						},
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusOK,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+						Body: dsl.Like(map[string]interface{}{
+							"total": dsl.Like(1),
+							"limit": dsl.Like(25),
+							"pages": dsl.Like(map[string]interface{}{
+								"current": dsl.Like(1),
+								"total":   dsl.Like(1),
+							}),
+							"cases": dsl.EachLike(map[string]interface{}{
+								"id":  dsl.Like(36),
+								"uId": dsl.Term("7000-8548-8461", `\d{4}-\d{4}-\d{4}`),
+								"donor": dsl.Like(map[string]interface{}{
+									"id":        dsl.Like(23),
+									"uId":       dsl.Term("7000-5382-4438", `\d{4}-\d{4}-\d{4}`),
+									"firstname": dsl.Like("Adrian"),
+									"surname":   dsl.Like("Kurkjian"),
+								}),
+								"caseSubtype": dsl.Term("pf", "hw|pf"),
+								"receiptDate": dsl.Term("12/05/2021", `\d{1,2}/\d{1,2}/\d{4}`),
+								"status":      dsl.Like("Perfect"),
+							}, 1),
+						}),
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+			expectedCases: []Case{{
+				ID:  36,
+				Uid: "7000-8548-8461",
+				Donor: Donor{
+					ID:        23,
+					Uid:       "7000-5382-4438",
+					Firstname: "Adrian",
+					Surname:   "Kurkjian",
+				},
+				SubType:     "pf",
+				ReceiptDate: SiriusDate{time.Date(2021, 5, 12, 0, 0, 0, 0, time.UTC)},
+				Status:      "Perfect",
+			}},
+			expectedPagination: &Pagination{
+				TotalItems:  1,
+				CurrentPage: 1,
+				TotalPages:  1,
+				PageSize:    25,
+			},
+		},
+		{
+			name:   "OK by status",
+			status: "Pending",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have a pending case assigned").
+					UponReceiving("A request to get my pending cases").
 					WithRequest(dsl.Request{
 						Method: http.MethodGet,
 						Path:   dsl.String("/api/v1/assignees/47/cases"),
@@ -73,6 +144,7 @@ func TestCasesByAssignee(t *testing.T) {
 								}),
 								"caseSubtype": dsl.Term("hw", "hw|pf"),
 								"receiptDate": dsl.Term("14/05/2021", `\d{1,2}/\d{1,2}/\d{4}`),
+								"status":      dsl.Like("Pending"),
 							}, 1),
 						}),
 					})
@@ -92,59 +164,12 @@ func TestCasesByAssignee(t *testing.T) {
 				},
 				SubType:     "hw",
 				ReceiptDate: SiriusDate{time.Date(2021, 5, 14, 0, 0, 0, 0, time.UTC)},
+				Status:      "Pending",
 			}},
 			expectedPagination: &Pagination{
 				TotalItems:  1,
 				CurrentPage: 1,
 				TotalPages:  1,
-				PageSize:    25,
-			},
-		},
-		{
-			name:   "Invalid status",
-			status: "Unsupported",
-			setup: func() {
-				pact.
-					AddInteraction().
-					Given("I have a pending case assigned").
-					UponReceiving("A request to get my cases by status").
-					WithRequest(dsl.Request{
-						Method: http.MethodGet,
-						Path:   dsl.String("/api/v1/assignees/47/cases"),
-						Query: dsl.MapMatcher{
-							"filter": dsl.String("caseType:lpa,active:true,status:Unsupported"),
-							"sort":   dsl.String("caseSubType:asc"),
-							"page":   dsl.String("1"),
-						},
-						Headers: dsl.MapMatcher{
-							"X-XSRF-TOKEN":        dsl.String("abcde"),
-							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
-							"OPG-Bypass-Membrane": dsl.String("1"),
-						},
-					}).
-					WillRespondWith(dsl.Response{
-						Status:  http.StatusOK,
-						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
-						Body: dsl.Like(map[string]interface{}{
-							"total": dsl.Like(0),
-							"limit": dsl.Like(25),
-							"pages": dsl.Like(map[string]interface{}{
-								"current": dsl.Like(1),
-								"total":   dsl.Like(0),
-							}),
-							"cases": []struct{}{},
-						}),
-					})
-			},
-			cookies: []*http.Cookie{
-				{Name: "XSRF-TOKEN", Value: "abcde"},
-				{Name: "Other", Value: "other"},
-			},
-			expectedCases: []Case{},
-			expectedPagination: &Pagination{
-				TotalItems:  0,
-				CurrentPage: 1,
-				TotalPages:  0,
 				PageSize:    25,
 			},
 		},
