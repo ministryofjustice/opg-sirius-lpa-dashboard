3ca717d7127872bab76e70c318c0480b58eee7f5
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-05-24 16:45:22 +0100

    Support filtering and pagination on case request
    When requesting a user's cases, support pagination and filtering to a specific status

--- internal/sirius/cases_by_assignee_test.go
+++ internal/sirius/cases_by_assignee_test.go
@@ -22,14 +22,17 @@ func TestCasesByAssignee(t *testing.T) {
 	defer pact.Teardown()
 
 	testCases := []struct {
-		name          string
-		setup         func()
-		cookies       []*http.Cookie
-		expectedCases []Case
-		expectedError error
+		name               string
+		status             string
+		setup              func()
+		cookies            []*http.Cookie
+		expectedCases      []Case
+		expectedPagination *Pagination
+		expectedError      error
 	}{
 		{
-			name: "OK",
+			name:   "OK",
+			status: "Pending",
 			setup: func() {
 				pact.
 					AddInteraction().
@@ -39,8 +42,9 @@ func TestCasesByAssignee(t *testing.T) {
 						Method: http.MethodGet,
 						Path:   dsl.String("/api/v1/assignees/47/cases"),
 						Query: dsl.MapMatcher{
-							"filter": dsl.String("caseType:lpa,status:Pending,active:true"),
+							"filter": dsl.String("caseType:lpa,active:true,status:Pending"),
 							"sort":   dsl.String("caseSubType:asc"),
+							"page":   dsl.String("1"),
 						},
 						Headers: dsl.MapMatcher{
 							"X-XSRF-TOKEN":        dsl.String("abcde"),
@@ -52,6 +56,12 @@ func TestCasesByAssignee(t *testing.T) {
 						Status:  http.StatusOK,
 						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
 						Body: dsl.Like(map[string]interface{}{
+							"total": dsl.Like(1),
+							"limit": dsl.Like(25),
+							"pages": dsl.Like(map[string]interface{}{
+								"current": dsl.Like(1),
+								"total":   dsl.Like(1),
+							}),
 							"cases": dsl.EachLike(map[string]interface{}{
 								"id":  dsl.Like(58),
 								"uId": dsl.Term("7000-2830-9492", `\d{4}-\d{4}-\d{4}`),
@@ -83,8 +93,61 @@ func TestCasesByAssignee(t *testing.T) {
 				SubType:     "hw",
 				ReceiptDate: SiriusDate{time.Date(2021, 5, 14, 0, 0, 0, 0, time.UTC)},
 			}},
+			expectedPagination: &Pagination{
+				TotalItems:  1,
+				CurrentPage: 1,
+				TotalPages:  1,
+				PageSize:    25,
+			},
 		},
-
+		{
+			name:   "Invalid status",
+			status: "Unsupported",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("I have a pending case assigned").
+					UponReceiving("A request to get my cases by status").
+					WithRequest(dsl.Request{
+						Method: http.MethodGet,
+						Path:   dsl.String("/api/v1/assignees/47/cases"),
+						Query: dsl.MapMatcher{
+							"filter": dsl.String("caseType:lpa,active:true,status:Unsupported"),
+							"sort":   dsl.String("caseSubType:asc"),
+							"page":   dsl.String("1"),
+						},
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusOK,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+						Body: dsl.Like(map[string]interface{}{
+							"total": dsl.Like(0),
+							"limit": dsl.Like(25),
+							"pages": dsl.Like(map[string]interface{}{
+								"current": dsl.Like(1),
+								"total":   dsl.Like(0),
+							}),
+							"cases": []struct{}{},
+						}),
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+			expectedCases: []Case{},
+			expectedPagination: &Pagination{
+				TotalItems:  0,
+				CurrentPage: 1,
+				TotalPages:  0,
+				PageSize:    25,
+			},
+		},
 		{
 			name: "Unauthorized",
 			setup: func() {
@@ -96,8 +159,9 @@ func TestCasesByAssignee(t *testing.T) {
 						Method: http.MethodGet,
 						Path:   dsl.String("/api/v1/assignees/47/cases"),
 						Query: dsl.MapMatcher{
-							"filter": dsl.String("caseType:lpa,status:Pending,active:true"),
+							"filter": dsl.String("caseType:lpa,active:true"),
 							"sort":   dsl.String("caseSubType:asc"),
+							"page":   dsl.String("1"),
 						},
 					}).
 					WillRespondWith(dsl.Response{
@@ -115,8 +179,9 @@ func TestCasesByAssignee(t *testing.T) {
 			assert.Nil(t, pact.Verify(func() error {
 				client, _ := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
 
-				cases, err := client.CasesByAssignee(getContext(tc.cookies), 47)
+				cases, pagination, err := client.CasesByAssignee(getContext(tc.cookies), 47, tc.status, 1)
 				assert.Equal(t, tc.expectedCases, cases)
+				assert.Equal(t, tc.expectedPagination, pagination)
 				assert.Equal(t, tc.expectedError, err)
 				return nil
 			}))
@@ -130,10 +195,10 @@ func TestCasesByAssigneeStatusError(t *testing.T) {
 
 	client, _ := NewClient(http.DefaultClient, s.URL)
 
-	_, err := client.CasesByAssignee(getContext(nil), 47)
+	_, _, err := client.CasesByAssignee(getContext(nil), 47, "", 2)
 	assert.Equal(t, StatusError{
 		Code:   http.StatusTeapot,
-		URL:    s.URL + "/api/v1/assignees/47/cases?filter=caseType:lpa,status:Pending,active:true&sort=caseSubType:asc",
+		URL:    s.URL + "/api/v1/assignees/47/cases?page=2&filter=caseType:lpa,active:true&sort=caseSubType:asc",
 		Method: http.MethodGet,
 	}, err)
 }
