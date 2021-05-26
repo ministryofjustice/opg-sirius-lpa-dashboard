606541b53e66db104b8247293c22005a28e2eee9
Greg Tyler <greg.tyler@digital.justice.gov.uk>
2021-05-26 14:31:23 +0100

    Add UserByEmail API endpoint
    So we can get the ID of the `manager@opgtest.com` user.

---
+++ internal/sirius/user_by_email.go
@@ -0,0 +1,43 @@
+package sirius
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	ID int
+}
+
+type authUserResponse struct {
+	ID int `json:"id"`
+}
+
+func (c *Client) UserByEmail(ctx Context, email string) (User, error) {
+	req, err := c.newRequest(ctx, http.MethodGet, fmt.Sprintf("/api/v1/users?email=%s", email), nil)
+	if err != nil {
+		return User{}, err
+	}
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return User{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		return User{}, ErrUnauthorized
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return User{}, newStatusError(resp)
+	}
+
+	var v authUserResponse
+	if err := json.NewDecoder(resp.Body).Decode(&v); err != nil {
+		return User{}, err
+	}
+
+	return User(v), err
+}
---
+++ internal/sirius/user_by_email_test.go
@@ -0,0 +1,122 @@
+package sirius
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/pact-foundation/pact-go/dsl"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserByEmail(t *testing.T) {
+	pact := &dsl.Pact{
+		Consumer:          "sirius-lpa-dashboard",
+		Provider:          "sirius",
+		Host:              "localhost",
+		PactFileWriteMode: "merge",
+		LogDir:            "../../logs",
+		PactDir:           "../../pacts",
+	}
+	defer pact.Teardown()
+
+	testCases := []struct {
+		name          string
+		setup         func()
+		cookies       []*http.Cookie
+		email         string
+		expectedUser  User
+		expectedError error
+	}{
+		{
+			name:  "OK",
+			email: "manager@opgtest.com",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("!Manager user exists").
+					UponReceiving("A request to get !Manager's ID").
+					WithRequest(dsl.Request{
+						Method: http.MethodGet,
+						Path:   dsl.String("/api/v1/users"),
+						Query: dsl.MapMatcher{
+							"email": dsl.String("manager@opgtest.com"),
+						},
+						Headers: dsl.MapMatcher{
+							"X-XSRF-TOKEN":        dsl.String("abcde"),
+							"Cookie":              dsl.String("XSRF-TOKEN=abcde; Other=other"),
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status:  http.StatusOK,
+						Headers: dsl.MapMatcher{"Content-Type": dsl.String("application/json")},
+						Body: dsl.Like(map[string]interface{}{
+							"id": dsl.Like(14),
+						}),
+					})
+			},
+			cookies: []*http.Cookie{
+				{Name: "XSRF-TOKEN", Value: "abcde"},
+				{Name: "Other", Value: "other"},
+			},
+			expectedUser: User{
+				ID: 14,
+			},
+		},
+
+		{
+			name:  "Unauthorized",
+			email: "manager@opgtest.com",
+			setup: func() {
+				pact.
+					AddInteraction().
+					Given("!Manager user exists").
+					UponReceiving("A request to get !Manager's ID without cookies").
+					WithRequest(dsl.Request{
+						Method: http.MethodGet,
+						Path:   dsl.String("/api/v1/users"),
+						Query: dsl.MapMatcher{
+							"email": dsl.String("manager@opgtest.com"),
+						},
+						Headers: dsl.MapMatcher{
+							"OPG-Bypass-Membrane": dsl.String("1"),
+						},
+					}).
+					WillRespondWith(dsl.Response{
+						Status: http.StatusUnauthorized,
+					})
+			},
+			expectedError: ErrUnauthorized,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tc.setup()
+
+			assert.Nil(t, pact.Verify(func() error {
+				client, _ := NewClient(http.DefaultClient, fmt.Sprintf("http://localhost:%d", pact.Server.Port))
+
+				user, err := client.UserByEmail(getContext(tc.cookies), tc.email)
+				assert.Equal(t, tc.expectedUser, user)
+				assert.Equal(t, tc.expectedError, err)
+				return nil
+			}))
+		})
+	}
+}
+
+func TestUserByEmailStatusError(t *testing.T) {
+	s := teapotServer()
+	defer s.Close()
+
+	client, _ := NewClient(http.DefaultClient, s.URL)
+
+	_, err := client.UserByEmail(getContext(nil), "someone@opgtest.com")
+	assert.Equal(t, StatusError{
+		Code:   http.StatusTeapot,
+		URL:    s.URL + "/api/v1/users?email=someone@opgtest.com",
+		Method: http.MethodGet,
+	}, err)
+}
