be950e6898a1466f9ec9d5a41fadea993304d929
Greg Tyler <gregtyler@users.noreply.github.com>
2022-08-31 14:17:04 +0100

    Move `/request-next-payment-task` endpoint (#224)
    To `/request-next-task`. With a more generic name, make it use the more generic `/api/v1/request-new-task` API endpoint.
    
    Update the related structs and tests. And give the docker image the correct tag so that it can be loaded into the Sirius stack.
    
    Fixes VEGA-1445 #minor

---
+++ internal/server/request_next_task_test.go
@@ -0,0 +1,69 @@
+package server
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ministryofjustice/opg-sirius-lpa-dashboard/internal/sirius"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockRequestNextTaskClient struct {
+	requestNextTask struct {
+		count   int
+		lastCtx sirius.Context
+		err     error
+	}
+}
+
+func (m *mockRequestNextTaskClient) RequestNextTask(ctx sirius.Context) error {
+	m.requestNextTask.count += 1
+	m.requestNextTask.lastCtx = ctx
+
+	return m.requestNextTask.err
+}
+
+func TestPostRequestNextTask(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockRequestNextTaskClient{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/path", nil)
+
+	err := requestNextTask(client)(w, r)
+	assert.Equal(RedirectError("/card-payments"), err)
+
+	assert.Equal(1, client.requestNextTask.count)
+	assert.Equal(getContext(r), client.requestNextTask.lastCtx)
+}
+
+func TestPostRequestNextTaskErrors(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockRequestNextTaskClient{}
+	client.requestNextTask.err = errors.New("err")
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/path", nil)
+
+	err := requestNextTask(client)(w, r)
+	assert.Equal(client.requestNextTask.err, err)
+}
+
+func TestBadMethodRequestNextTask(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockRequestNextTaskClient{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("DELETE", "/path", nil)
+
+	err := requestNextTask(client)(w, r)
+
+	assert.Equal(StatusError(http.StatusMethodNotAllowed), err)
+
+	assert.Equal(0, client.requestNextTask.count)
+}
